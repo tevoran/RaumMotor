121ca0e342ba11e473f109c6c630adbfd60d66f0
tevoran <j.pfeiffer93@gmx.de>
2022-12-18 13:30:25 +0100

    added scripts for installing the current dependencies

---
+++ build-system/linux/install-dependencies-debian.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#installing all dependencies on Debian based systems like Ubuntu, Mint or Pop!_OS
+
+sudo apt install gcc cmake make 
---
+++ build-system/linux/install-dependencies-gentoo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#installing all dependencies on Gentoo
+
+emerge -qa sys-devel/gcc sys-devel/make dev-util/cmake
